83e8512199a15c12cfd454571f479efd7ee68eca
phamngocduc2000 <116892534+phamngocduc2000@users.noreply.github.com>
2023-11-12 20:22:35 +0700

    Add files via upload

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,7 +1,8 @@
 import React from "react";
 import './AboutUs.scss'
 import pic1 from "../../assets/images/AboutUs/1.webp";
-import pic2 from "../../assets/images/AboutUs/2.jpg";
+import pic2 from "../../assets/images/AboutUs/2.jfif";
+import video2 from "../../assets/images/AboutUs/videoAboutUs.mp4";
 import pic3 from "../../assets/images/AboutUs/3.webp";
 import pic4 from "../../assets/images/AboutUs/4.webp";
 import pic5 from "../../assets/images/AboutUs/5.png";
@@ -19,7 +20,10 @@ const AboutUs = () => {
                 <img src={pic1} alt="pic1" className="aboutus-img"/>
             </section>
             <section className="aboutus">
-                <img src={pic2} alt="pic2" className="aboutus-img"/>
+                <video controls className="video" poster={pic2}>
+                <source src={video2} type="video/mp4"  />
+                Your browser does not support the video tag.
+                </video>
                 <h5 className="text-aboutus">We made a video to show you who we are and what we're becoming. Check it out!</h5>
             </section>
             <section className="aboutus">
@@ -32,13 +36,13 @@ const AboutUs = () => {
                 <img src={pic5} alt="pic5" className="aboutus-img"/>
             </section>
             <section className="aboutus">
-                <img src={pic6} alt="pic6" className="aboutus-img"/>
+                <img src={pic6} alt="pic6" className="aboutus-img-full"/>
             </section>
             <section className="aboutus">
-                <img src={pic7} alt="pic7" className="aboutus-img"/>
+                <img src={pic7} alt="pic7" className="aboutus-img-full"/>
             </section>
             <section className="aboutus">
-                <img src={pic8} alt="pic8" className="aboutus-img"/>
+                <img src={pic8} alt="pic8" className="aboutus-img-full"/>
             </section>
             <section>
                 <img src={pic9} alt="pic9" className="aboutus-img"/>

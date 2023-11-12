adbe920a4750dbf3940ca924c95a7b800e9548ad
Duc <phamngocduc2000@gmail.com>
2023-11-12 21:31:47 +0700

    new code

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,26 +1,30 @@
 import ReactPaginate from "react-paginate";
 import "./pagination.scss";
+import { useEffect } from "react";
 
 function Pagination({ pageCount, handPageClick }) {
-  return (
-    <div>
-      <ReactPaginate
-        breakLabel="..."
-        breakClassName="break-class"
-        activeClassName="activepagination"
-        containerClassName={"pagination"}
-        pageLinkClassName="buttonPage"
-        previousLinkClassName="buttonPage"
-        nextLinkClassName="buttonPage"
-        nextLabel="next >"
-        onPageChange={handPageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-      />
-    </div>
-  );
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    return (
+        <div>
+            <ReactPaginate
+                breakLabel="..."
+                breakClassName="break-class"
+                activeClassName="activepagination"
+                containerClassName={"pagination"}
+                pageLinkClassName="buttonPage"
+                previousLinkClassName="buttonPage"
+                nextLinkClassName="buttonPage"
+                nextLabel="next >"
+                onPageChange={handPageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+            />
+        </div>
+    );
 }
 
 export default Pagination;
